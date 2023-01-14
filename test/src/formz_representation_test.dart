977953136604e49f23f1285aeaa7369bbc9edbf2
Luis <lespinerua@gmail.com>
2023-01-14 19:40:29 -0400

    feat: password representation

--- test/src/formz_representation_test.dart
+++ test/src/formz_representation_test.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: prefer_const_constructors
 
-import 'package:formz_representation/src/inputs/email.dart';
+import 'package:formz_representation/formz_representation.dart';
 import 'package:test/test.dart';
 
 void main() {
@@ -54,6 +54,71 @@ void main() {
       expect(
         EmailInput.dirty(value: 'lesp').toPure.displayError,
         isNull,
+      );
+    });
+  });
+
+  group('PasswordInput', () {
+    test('can be instantiated', () {
+      expect(PasswordInput.pure(), isNotNull);
+    });
+
+    test('pure is true when super.pure is used', () {
+      expect(PasswordInput.pure().pure, isTrue);
+    });
+
+    test('pure is false when super.dirty is used', () {
+      expect(PasswordInput.dirty().pure, isFalse);
+    });
+
+    test('displayError is null when super.pure is used', () {
+      expect(PasswordInput.pure().displayError, isNull);
+    });
+
+    test('displayError is null when super.pure is used with an initial value',
+        () {
+      expect(PasswordInput.pure(value: 'luis').displayError, isNull);
+    });
+
+    test('displayError is not null when super.dirty is used', () {
+      expect(PasswordInput.dirty().displayError, isNotNull);
+    });
+
+    test('PasswordValidationError is not empty', () {
+      expect(PasswordInput.dirty().displayError, PasswordValidationError.empty);
+    });
+
+    test('PasswordValidationError is less than 8 characters', () {
+      expect(
+        PasswordInput.dirty(value: '1234567').displayError,
+        PasswordValidationError.invalidCharactersNumber,
+      );
+    });
+    test('PasswordValidationError is not contains lowercase', () {
+      expect(
+        PasswordInput.dirty(value: 'ABCDEFG1').displayError,
+        PasswordValidationError.notContainsLowercase,
+      );
+    });
+
+    test('PasswordValidationError is not contains uppercase', () {
+      expect(
+        PasswordInput.dirty(value: 'abcdefg1').displayError,
+        PasswordValidationError.notContainsUppercase,
+      );
+    });
+
+    test('PasswordValidationError is not contains digits', () {
+      expect(
+        PasswordInput.dirty(value: 'ABCdefg.').displayError,
+        PasswordValidationError.notContainsDigits,
+      );
+    });
+
+    test('PasswordValidationError is not contains special characters', () {
+      expect(
+        PasswordInput.dirty(value: 'ABCdef12').displayError,
+        PasswordValidationError.notContainsSpecialCharacters,
       );
     });
   });
