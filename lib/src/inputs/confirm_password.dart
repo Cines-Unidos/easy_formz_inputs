d38ac1660818718e4b56cd5ce6aa9d2383ac85eb
Luis <lespinerua@gmail.com>
2023-01-16 22:42:57 -0400

    feat: confirm password input

---
+++ lib/src/inputs/confirm_password.dart
@@ -0,0 +1,50 @@
+import 'package:formz/formz.dart';
+
+/// [ConfirmPasswordInput] validation error
+enum ConfirmPasswordValidationError {
+  /// ConfirmPassword is empty
+  empty,
+
+  /// ConfirmPassword no coincidence
+  noCoincidence
+}
+
+/// Password representation class which extends [FormzInput] and provides
+/// validation logic.
+class ConfirmPasswordInput
+    extends FormzInput<String, ConfirmPasswordValidationError> {
+  const ConfirmPasswordInput.pure({
+    String value = '',
+    required this.password,
+  }) : super.pure(value);
+
+  /// Dirty representation of [ConfirmPasswordInput]
+  const ConfirmPasswordInput.dirty({
+    String value = '',
+    required this.password,
+  }) : super.dirty(value);
+
+  /// Password value to confirm
+  final String password;
+
+  /// Display error if the [ConfirmPasswordInput] is not valid
+  /// and has been modified
+  ConfirmPasswordValidationError? get displayError => pure ? null : error;
+
+  /// Convert [ConfirmPasswordInput] to pure
+  ConfirmPasswordInput get toPure =>
+      ConfirmPasswordInput.pure(value: value, password: password);
+
+  @override
+  ConfirmPasswordValidationError? validator(String? value) {
+    if (value == null || value.isEmpty) {
+      return ConfirmPasswordValidationError.empty;
+    }
+
+    if (value != password) {
+      return ConfirmPasswordValidationError.noCoincidence;
+    }
+
+    return null;
+  }
+}
